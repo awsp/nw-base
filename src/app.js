850602a28359c3c738956904f82c657fbe752fc7
awsp <anthonyspwu@gmail.com>
2015-05-25 23:18:18 -1000

    v0.5 development

--- src/app.js
+++ src/app.js
@@ -1,4 +1,14 @@
-var app = angular.module('nwbase', []);
+var gui = require('nw.gui');
+var win = gui.Window.get();
+var app = angular.module('nwbase', ['kendo.directives']);
 
+// Global Exports
 global.$ = $;
 global._ = _;
+
+
+app.controller('MainController', function () {
+  this.close = function () {
+    win.close();
+  }
+});
